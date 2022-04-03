86e8e02c4e32dbf4b53daa63160dd97e23d92979
Muhammad Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2022-04-03 17:35:39 +0500

    Update apis.dart

--- ut/lib/database/apis.dart
+++ ut/lib/database/apis.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'package:uet_tests/main.dart';
+import 'package:uet_tests/database/models.dart';
 import 'package:uet_tests/screens/login_success/login_success_screen.dart';
+import 'package:uet_tests/screens/sign_up/sign_up_screen.dart';
 import 'package:uet_tests/screens/complete_profile/complete_profile_screen.dart';
 
 Future<http.Response> post(String query, Map data) async {
@@ -44,23 +46,30 @@ Future<http.Response> get(String query) async {
   return response;
 }
 
-void getLoginManagerfromUID(String uid) async {
-  String query = 'https://uet-test.herokuapp.com/api/signup';
+Login_Manager parseLoginManager(String responseBody) {
+  final parsedJson = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return Login_Manager.fromJson(parsedJson);
+}
 
-  Map<String, dynamic> map = {"uid": uid};
+List<Login_Manager> parseLoginManagerList(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed
+      .map<Login_Manager>((json) => Login_Manager.fromJson(json))
+      .toList();
+}
 
-  Uri.https(
-      'https://uet-test.herokuapp.com', '/api/getLoginManagerFromUID/', map);
+Future<http.Response> getLoginManagerfromUID() async {
+  String uid = auth.currentUser!.uid;
+  String query =
+      'https://uet-test.herokuapp.com/api/getLoginManagerFromUID/$uid/';
 
   http.Response responseRecieved = await get(query);
 
   print(responseRecieved.body);
 
-  if (responseRecieved.statusCode == 201) {
-    print("New Account Created");
-  } else {
-    print("Already Present");
-  }
+  Login_Manager login_manager = parseLoginManager(responseRecieved.body);
+
+  return responseRecieved;
 }
 
 void signup(Map data, BuildContext context) async {
@@ -68,21 +77,35 @@ void signup(Map data, BuildContext context) async {
 
   http.Response responseRecieved = await post(query, data);
 
+  /*
   if (responseRecieved.statusCode == 201) {
     // Created
-
     Navigator.pushReplacementNamed(context, CompleteProfileScreen.routeName);
     print("New Account Created");
-  } else {
+  } else if (responseRecieved.statusCode == 208) {
     // Already Present
-    getLoginManagerfromUID(data["GoogleSiginUID"]);
+
+    http.Response loginMangerResponse = (data["GoogleSiginUID"]);
+    if (loginMangerResponse.statusCode == 302) {
+    } else {}
+
     Navigator.pushReplacementNamed(context, LoginSuccessScreen.routeName);
     print("Already Present");
-  }
+  }*/
+  Navigator.pushReplacementNamed(context, CompleteProfileScreen.routeName);
 }
 
 void completeSignup(Map data, BuildContext context) async {
-  String query = 'https://uet-test.herokuapp.com/api/signup';
+  http.Response response = await getLoginManagerfromUID();
+
+  if (response.statusCode == 404) {
+    Navigator.pushReplacementNamed(context, SignUpScreen.routeName);
+    return;
+  }
+
+  Login_Manager login_manager = parseLoginManager(response.body);
+
+  String query = 'https://uet-test.herokuapp.com/api/user';
 
   http.Response responseRecieved = await post(query, data);
 
